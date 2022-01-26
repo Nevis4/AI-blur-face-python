32d493be5063f99edfac833a94c76c9572d31f9c
Nevis4 <przemyslaw.malara@icloud.com>
2022-01-26 13:27:57 +0100

    creating a face detection and displaying it in a rectangle

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import cv2
 capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
+faceCascade = cv2.CascadeClassifier('/Users/przemyslawmalara/programowanie/pyton/Machine_Learning/Open_CV/haarcascades/haarcascade_frontalface_default.xml')
 while True:
     _, frame = capture.read()
+    greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        greyscale, scaleFactor=1.2, minNeighbors=5, minSize=(50, 50)
+    )
+
+    for x,y,face_width, face_height in faces:
+        cv2.rectangle(frame, (x,y), (x+face_width, y+face_height), (255,0,0),5)
+       
     cv2.imshow('frame', frame)
+    cv2.imshow('grey', greyscale)
 
     key = cv2.waitKey(50)
     if key == 27:
